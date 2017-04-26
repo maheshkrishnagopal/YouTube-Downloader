fe0b967cb70f7d05cc8f96b781d623d07df84f21
Maheshkrishna A G <maheshkrishnagopal@gmail.com>
2017-04-26 21:12:40 +0530

    Add files via upload
    This is the python file which gets the Youtube URL and downloads the video in mp4 format.

---
+++ YouTubeDownloader.py
@@ -0,0 +1,39 @@
+'''
+Name of the Program: YouTube Downloader
+Scope: This program has been created with best module available in Python. But, this can also be optimized and can get better structure.
+Author: Maheshkrishna A G
+Module Used: Pafy
+Version: 0.1
+Description: This Python program downloads Youtube videos to your local directory, when the URL is given.
+Input: Youtube video URL.
+Date: 26-04-2017
+
+'''
+from pafy import *;
+
+def status(total, recvd, ratio, rate, eta):
+    print(recvd, ratio, eta);
+    
+
+url=input("Enter the YouTube url to download: ");
+video=pafy.new(url);
+print("Ready to download, " + video.title + "? (y/n)");
+action=input();
+if (action=='y'):
+    try:
+        print ("Downloading !!! " + video.title);
+        best=video.getbest(preftype="mp4");
+        filename=best.download(quiet=True, callback=status, filepath="C:\\Users\\VAIO\\Desktop");
+        print("Download Completed!!! Please check in this path - C:\\Users\\VAIO\\Desktop");
+        exit=input("Press any key to Exit!");
+    except:
+        print ("Unexpected Error Occured, hence download failed!");
+        exit=input("Enter any key to Exit:");
+else:
+    print("Exiting the program:");
+    print("Press any key to exit");
+    exit();
+
+    
+    
+
